dab467b32b5a10ff9423fc1351b80c837bf36aba
MonGroy <mongtualmr@gmail.com>
2024-06-04 17:18:07 +0900

    Git init

---
+++ src/main/java/org/example/spartaboard/SpartaBoardApplication.java
@@ -0,0 +1,13 @@
+package org.example.spartaboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartaBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/spartaboard/SpartaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spartaboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
