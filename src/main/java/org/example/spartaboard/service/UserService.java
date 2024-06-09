147b279509c3b3280901ec5a301694473a402a91
Been777 <o7.been@gmail.com>
2024-06-10 03:55:27 +0900

    Comment : Filter, Service 주석 추가

--- src/main/java/org/example/spartaboard/service/UserService.java
+++ src/main/java/org/example/spartaboard/service/UserService.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService {  // 애플리케이션의 비즈니스 로직을 처리하는 서비스 클래스
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
     private final PasswordEncoder passwordEncoder;
